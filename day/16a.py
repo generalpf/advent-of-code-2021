65640b658398176f2ac01adcb05f3acdc8727122
Ryan Walberg <generalpf@gmail.com>
2021-12-16 22:32:36 -0500

    day 16 💾

---
+++ day/16a.py
@@ -0,0 +1,69 @@
+literal = "D2FE28"
+operator_v3_tx = "8A004A801A8002F478"
+operator_v4_tx = "620080001611562C8802118E34"
+operator_v4_alt_tx = "C0015000016115A2E0802F182340"
+nested_operator_packet = "A0016C880162017C3686B18A3D4780"
+puzzle_input = "A20D5080210CE4BB9BAFB001BD14A4574C014C004AE46A9B2E27297EECF0C013F00564776D7E3A825CAB8CD47B6C537DB99CD746674C1000D29BBC5AC80442966FB004C401F8771B61D8803D0B22E4682010EE7E59ACE5BC086003E3270AE4024E15C8010073B2FAD98E004333F9957BCB602E7024C01197AD452C01295CE2DC9934928B005DD258A6637F534CB3D89A944230043801A596B234B7E58509E88798029600BCF5B3BA114F5B3BA10C9E77BAF20FA4016FCDD13340118B929DD4FD54E60327C00BEB7002080AA850031400D002369400B10034400F30021400F20157D804AD400FE00034E000A6D001EB2004E5C00B9AE3AC3C300470029091ACADBFA048D656DFD126792187008635CD736B3231A51BA5EBDF42D4D299804F26B33C872E213C840022EC9C21FFB34EDE7C559C8964B43F8AD77570200FC66697AFEB6C757AC0179AB641E6AD9022006065CEA714A4D24C0179F8E795D3078026200FC118EB1B40010A8D11EA27100990200C45A83F12C401A8611D60A0803B1723542889537EFB24D6E0844004248B1980292D608D00423F49F9908049798B4452C0131006230C14868200FC668B50650043196A7F95569CF6B663341535DCFE919C464400A96DCE1C6B96D5EEFE60096006A400087C1E8610A4401887D1863AC99F9802DC00D34B5BCD72D6F36CB6E7D95EBC600013A88010A8271B6281803B12E124633006A2AC3A8AC600BCD07C9851008712DEAE83A802929DC51EE5EF5AE61BCD0648028596129C3B98129E5A9A329ADD62CCE0164DDF2F9343135CCE2137094A620E53FACF37299F0007392A0B2A7F0BA5F61B3349F3DFAEDE8C01797BD3F8BC48740140004322246A8A2200CC678651AA46F09AEB80191940029A9A9546E79764F7C9D608EA0174B63F815922999A84CE7F95C954D7FD9E0890047D2DC13B0042488259F4C0159922B0046565833828A00ACCD63D189D4983E800AFC955F211C700"
+
+tx: list = list(puzzle_input)
+bits: str = None
+bit_index: int = 0
+
+
+def read_bits(bit_count: int) -> str:
+    global tx, bits, bit_index
+    
+    while bits is None or bit_count > len(bits):
+        new_word = bin(int(tx.pop(0), base = 16)).removeprefix("0b").zfill(4)
+        if bits is None:
+            bits = new_word
+        else:
+            bits += new_word
+
+    bit_index += bit_count
+    return_this = bits[:bit_count]
+    bits = bits[bit_count:]
+    return return_this
+
+
+def read_number(bit_count: int) -> int:
+    return int(read_bits(bit_count), base = 2)
+
+
+def read_literal() -> int:
+    number_so_far = ""
+    while True:
+        quint = read_bits(5)
+        number_so_far += quint[1:]
+        if quint[0] == "0":
+            return int(number_so_far, base = 2)
+
+
+version_sum = 0
+
+
+def read_packet() -> int:
+    global version_sum
+
+    packet_version = read_number(3)
+    version_sum += packet_version
+    packet_type = read_number(3)
+    if packet_type == 4:
+        return read_literal()
+    else:
+        length_type = read_number(1)
+        if length_type == 0:
+            bit_length = read_number(15)
+            before = bit_index
+            while bit_index < before + bit_length:
+                packet = read_packet()
+        elif length_type == 1:
+            subpacket_count = read_number(11)
+            for i in range(subpacket_count):
+                packet = read_packet()
+    return 0
+
+
+read_packet()
+
+print(version_sum)
